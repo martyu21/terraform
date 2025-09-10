d7cb5f4368f0e8d065d4f83d16eba43903e65bc5
Martyn Little <martyn.little@outlook.com>
2025-09-10 22:23:47 +0100

    update vars

--- modules/azure_vm/main.tf
+++ modules/azure_vm/main.tf
@@ -42,12 +42,12 @@ resource "azurerm_network_interface" "lab_nic" {
 }
 
 resource "azurerm_windows_virtual_machine" "lab_vm" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.lab.name
-  location            = var.location
-  size                = var.vm_size
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
+  name                  = var.vm_name
+  resource_group_name   = azurerm_resource_group.lab.name
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
   network_interface_ids = [azurerm_network_interface.lab_nic.id]
 
   os_disk {
