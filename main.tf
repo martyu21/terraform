edd87882dd366d15ab832f1ce64fa86be529c767
Martyn Little <martyn.little@outlook.com>
2025-09-10 21:01:56 +0100

    removed hyper-v

--- main.tf
+++
@@ -1,8 +0,0 @@
-module "lab_vm" {
-  source       = "./modules/hyperv_vm"
-  vm_name      = var.vm_name
-  memory_mb    = var.memory_mb
-  cpu_count    = var.cpu_count
-  image_path   = var.image_path
-  switch_name  = var.switch_name
-}
