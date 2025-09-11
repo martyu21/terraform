0e994f2ff4f80e1879547bd7737a992d207e17a1
Martyn Little <martyn.little@outlook.com>
2025-09-11 23:14:30 +0100

    add sub id

--- modules/azure_vm/main.tf
+++ modules/azure_vm/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
   client_id       = var.client_id
   client_secret   = var.client_secret
-  subscription_id = 'e8645d64-cebf-417b-981c-4b3368e388f2'
+  subscription_id = "e8645d64-cebf-417b-981c-4b3368e388f2"
   tenant_id       = var.tenant_id
 
 
