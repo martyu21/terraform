f037947e180e568dbdb7fdeab5551c2e1d43c29b
Martyn Little <martyn.little@outlook.com>
2025-11-06 21:08:34 +0000

    renamed and split out var files

--- modules/azure_vm/networks.tf
+++ modules/azure_vm/networks.tf
@@ -1,9 +1,4 @@
 
-resource "azurerm_resource_group" "lab" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_virtual_network" "lab_vnet" {
   name                = "lab-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -30,24 +25,3 @@ resource "azurerm_network_interface" "lab_nic" {
   }
 }
 
-resource "azurerm_windows_virtual_machine" "lab_vm" {
-  name                  = var.vm_name
-  resource_group_name   = azurerm_resource_group.lab.name
-  location              = var.location
-  size                  = var.vm_size
-  admin_username        = var.admin_username
-  admin_password        = var.admin_password
-  network_interface_ids = [azurerm_network_interface.lab_nic.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2022-datacenter"
-    version   = "latest"
-  }
-}
