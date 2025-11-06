7b54a1e2ebfc5829224862814e4e21afe3c21491
Martyn Little <martyn.little@outlook.com>
2025-11-06 21:46:12 +0000

    Add outputs

--- modules/azure_vm/outputs.tf
+++ modules/azure_vm/outputs.tf
@@ -1,3 +1,7 @@
 output "vm_private_ip" {
   value = azurerm_network_interface.lab_nic.private_ip_address
 }
+
+output "virtual_machine_name" {
+  value = azurerm_windows_virtual_machine.lab_vm.computer_name
+}
